f85ef208dc8769c1ae4c0964eef5fed14dcd215b
rguerrerop <rguerrerop@DESKTOP-PJ9HHPD>
2018-12-19 16:01:12 -0600

    Se agregó el sitio

---
+++ EMWebApi/EM.Web/app/app.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use-strict'
+
+    var app = angular.module('EM', [
+                                    'ui.router',
+                                    'ui.bootstrap'
+    
+
+    ]);
+
+})();
